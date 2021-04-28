3517881babf68b7203d9498852201fc9c57d42c9
Krist Wongsuphasawat <krist.wongz@gmail.com>
2021-04-27 23:22:50 -0700

    Add d3 example

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,3 @@
 import "./styles.css";
 import "./example";
 
-// import { render } from "react-dom";
-
-// import App from "./App";
-// import "./example/rough";
-
-// const rootElement = document.getElementById("root");
-// render(<App />, rootElement);
